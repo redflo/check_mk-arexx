d60f4034d5914e5644f626b28a7716da8dc8a6da
redflo <flo@redflo.de>
2020-03-22 00:56:43 +0100

    Make plugin more flexible, dont use saveint/savefloat as recommended.

--- local/share/check_mk/checks/arexx_sensors.py
+++ local/share/check_mk/checks/arexx_sensors.py
@@ -6,34 +6,34 @@ import time
 def inventory_arexx_sensors(info, req_sensor_type):
     localtime = int(time.time())
     for sensorid,value,stime,signallevel,sensor_type,name in info:
-        if sensor_type == req_sensor_type and localtime-saveint(stime) < 900:
+        if sensor_type in req_sensor_type and abs(localtime-int(stime)) < 900:
             yield "%s %s" % (sensorid,name), None
 
 def check_arexx_sensors(item, params, info, sensor_type):
     localtime = int(time.time())
     sensorid = item.split(' ')[0]
     for line in info:
-        if line[0] == sensorid and line[4] == sensor_type and localtime-saveint(line[2]) < 300:
+        if line[0] == sensorid and line[4] in sensor_type and abs(localtime-int(line[2])) < 300:
             value=line[1].split(" ")[0]
             return value, line[5], sensorid
     return None,None,None
 
 def check_arexx_sensors_temp(item, params, info):
-    value,description,sensorid=check_arexx_sensors(item, params, info,"Temperature")
+    value,description,sensorid=check_arexx_sensors(item, params, info,["Temperature"])
     if not value == None:
-        return check_temperature(savefloat(value),params, "arexx_temp_%s" % sensorid)
+        return check_temperature(float(value),params, "arexx_temp_%s" % sensorid)
     
 def check_arexx_sensors_humidity(item, params, info):
-    value,description,sensorid=check_arexx_sensors(item, params, info,"Relative Humidity")
+    value,description,sensorid=check_arexx_sensors(item, params, info,["Humidity","Relative Humidity"])
     if not value == None:
-        return check_humidity(savefloat(value),params)
+        return check_humidity(float(value),params)
     
 
 # check declaration
 check_info["arexx_sensors.temp"] = {
     "service_description"     : "Temperature %s",
     "check_function"          : check_arexx_sensors_temp,
-    "inventory_function"      : lambda info: inventory_arexx_sensors(info,'Temperature'),
+    "inventory_function"      : lambda info: inventory_arexx_sensors(info,['Temperature']),
     'has_perfdata'            : True,
     "group"                   : "temperature",
     "includes"                : [ "temperature.include" ],
@@ -42,7 +42,7 @@ check_info["arexx_sensors.temp"] = {
 check_info["arexx_sensors.humidity"] = {
     "service_description"     : "Humidity %s",
     "check_function"          : check_arexx_sensors_humidity,
-    "inventory_function"      : lambda info: inventory_arexx_sensors(info,'Relative Humidity'),
+    "inventory_function"      : lambda info: inventory_arexx_sensors(info,['Humidity','Relative Humidity']),
     'has_perfdata'            : True,
     "group"                   : "humidity",
     "includes"                : [ "humidity.include" ],
